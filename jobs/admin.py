500728cc81443d135d56bad859d25ac472756e36
Darrel Manuel <john.manuel@lusfiber.net>
2020-01-11 14:48:04 -0600

    Added bootstrap and jobs are showing

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+# Import the class Job from models.py
 from .models import Job
 
 admin.site.register(Job)
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+# import Job object from models
+from .models import Job
 
-# Create your views here.
+def home(request):
+    # get all of the objects (jobs) from Job
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs':jobs})
